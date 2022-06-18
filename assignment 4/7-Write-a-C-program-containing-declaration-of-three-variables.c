99ef5a15d86f9f92d8170d37fcb8fbe66c0b5fe8
devendra dhare <devendradhare03@gmail.com>
2022-06-18 20:52:22 +0530

    assignment 4 is complite

---
+++ assignment 4/7-Write-a-C-program-containing-declaration-of-three-variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a = 5;
+    float b = 5.5;
+    char c = 'a';
+    printf("a = %d\nb = %f\nc = %c",a,b,c);
+    getch();
+}
---
+++ assignment 4/9-Write-a-C-program-to-print-character-stored-in-a-char-variable-also-print-its-ASCII-code.c
@@ -0,0 +1,9 @@
+// 9. Write a C program to print character stored in a char variable, also print its ASCII code.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a='@';
+    printf("%c %d",a,a);
+    getch();
+}
